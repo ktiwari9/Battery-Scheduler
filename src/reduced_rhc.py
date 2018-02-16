813b0a6fc04a48bef08ed0bd7cf5e919e38fd2a7
milanmt <milanmariya@gmail.com>
2018-02-16 19:05:31 +0300

    Including reduced receding horizon control code

---
+++ src/reduced_rhc.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+
+import subprocess
+import reduced_rhc_script
+#import rhc_mod_parse
+import rrhc_prism_parse
+import rewards_uncertain_hk
+import time
+import numpy as np
+
+if __name__ == '__main__':
+    ur = rewards_uncertain_hk.uncertain_rewards(True)
+    clusters, prob = ur.get_rewards()
+    #path_to_directory = ##TO BE ADDED 
+    #charge_model, discharge_model = battery_data.get_battery_model(path_to_directory)
+    charge_model = 'ab'
+    discharge_model = 'cd'
+    cl_id =[]
+    sample_reward = []
+    actual_reward = []
+    exp_reward = []
+    #main_path = roslib.packages.get_pkg_dir('battery_scheduler')
+    #path_rew = main_path +'/data/sample_rewards'
+    main_path = '/home/milan/workspace/strands_ws/src/battery_scheduler'
+    path_rew = main_path + '/data/sample_rewards'
+    with open(path_rew,'r') as f:
+        for line in f:
+            cl_id.append(int(line.split(' ')[0]))
+            sample_reward.append(float(line.split(' ')[1]))
+            actual_reward.append(float(line.split(' ')[2]))
+            exp_reward.append(float(line.split(' ')[3]))
+    
+    init_battery = 70
+    init_charging = 1
+    init_cluster = cl_id[0]
+
+    reward = []
+    time_v = []
+    battery = []
+    charging = []
+    action = []
+
+    battery.append(init_battery)
+    charging.append(init_charging)
+    output_path = main_path + '/data/rrhc_aug3'
+    model_path = main_path + '/models/'
+    
+    t1 = time.time()
+    with open(output_path, 'w') as fw:
+        fw.write('time charging battery action matched_reward actual_reward exp_reward  cluster_vals prob_vals\n')
+        for t in range(48*3):     # 48*3 for 3 days
+            rhc_pm = reduced_rhc_script.make_model('rrhc.prism', t, init_battery, init_charging, init_cluster, clusters, prob, charge_model, discharge_model)
+            fw.write('{0} {1} {2} '.format(t, init_charging, init_battery))
+            subprocess.call('./prism '+model_path+'rrhc.prism '+model_path+ 'model_prop.props -exportadv '+model_path+'rrhc.adv -exportprodstates '+model_path+'rrhc.sta -exporttarget '+model_path+'rrhc.lab',cwd='/home/milan/prism-svn/prism/bin',shell=True)
+            rhc_pp = rrhc_prism_parse.parse_model(['rrhcpre1.adv', 'rrhc.sta', 'rrhc.lab'], t,  sample_reward, rhc_pm.clusters)
+            next_state = rhc_pp.get_next_state(rhc_pp.initial_state)
+            
+            action.append(next_state[2])
+            battery.append(next_state[0][0])
+            charging.append(next_state[0][1])
+            time_v.append(t)
+
+            if next_state[2] == 'gather_reward':
+                reward.append(actual_reward[t])
+            else:
+                reward.append(0)
+
+            fw.write('{0} {1} {2} {3}'.format(next_state[2], sample_reward[t], actual_reward[t], exp_reward[t])) 
+            for cl in rhc_pm.clusters[0]:
+                fw.write(' {0}'.format(cl))
+            for p in rhc_pm.prob[0]:
+                fw.write(' {0}'.format(p))
+            fw.write('\n')
+            init_battery = int(next_state[0][0])
+            init_charging = int(next_state[0][1])
+            init_cluster = int(next_state[1])
+           
+    print 'Time taken: ', (time.time()-t1)
+    print 'Reward Day 1: ', np.sum(np.array(reward[:48]))
+    print 'Reward Day 2: ', np.sum(np.array(reward[48:96]))
+    print 'Reward Day 3: ', np.sum(np.array(reward[96:144]))
+    print 'Reward Day 4: ', np.sum(np.array(reward[144:]))
+    print 'Total: ', np.sum(np.array(reward))
+    
---
+++ src/rrhc_prism_parse.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sys
+import os
+import roslib
+ 
+
+class parse_model:
+
+    def __init__(self, filenames, current_time, sample_reward, clusters):
+        path = '/home/milan/workspace/strands_ws/src/battery_scheduler/models/'
+        #path = roslib.packages.get_pkg_dir('battery_scheduler') + '/models/'
+        for name in filenames:
+            if name[-4:] == '.lab':
+                with open(path+name, 'r') as label_file:
+                    self.labels =[]
+                    for line in label_file.readlines():
+                        if 'init' not in line:
+                            self.labels.append(line[:-1].split(': '))
+                        
+            elif name[-4:] == '.sta':
+                with open(path+name, 'r') as state_file:
+                    self.states = dict()
+                    for line in state_file.readlines():
+                        if '_da' not in line or 'battery' not in line:
+                            state = line[:-2].split(':(')
+                            s = state[1].split(',')
+                            self.states.update({state[0] : (s[1], s[2], s[3], s[4])})
+    
+            elif name[-4:] == '.adv':
+                with open(path+name, 'r') as adv_file: 
+                    self.policy = dict()
+                    for line in adv_file:
+                        array = line[:-1].split(' ')
+                        if len(array) > 2:
+                            if array[0] not in self.policy:
+                                l = []                
+                            else:
+                                l = self.policy[array[0]]
+                            l.append(array[1:])
+                            self.policy.update({array[0] : l})
+            
+        self.initial_state = self.get_initial_state()
+        self.sample_reward = sample_reward
+        self.current_t = current_time
+        self.clusters = clusters
+        
+        
+    def get_initial_state(self):
+        for element in self.labels:
+            if int(element[1]) == 0:
+                initial_state = element[0] 
+        return initial_state
+        
+    
+    # when no observed state
+    def get_next_state(self, current_state):
+        possible_states = self.policy[current_state]
+        t_next = self.current_t+1
+
+        if t_next < len(self.sample_reward):
+            matched_reward = self.sample_reward[t_next]  # the reward determined by the cluster for next time int
+
+            cl_no = len(self.clusters[0])
+            for i in range(len(self.clusters[1])):
+                #print matched_reward, float(self.clusters[1][i])
+                #print type(matched_reward), type(self.clusters[1][i])
+                if round(matched_reward) == round(self.clusters[1][i]):
+                    req_id = cl_no + i
+                    next_id = i
+                    break
+
+            if len(possible_states)>1:
+                next_id = 0
+                for ns_p_a in possible_states:
+                    next_cl_id = int(self.states[ns_p_a[0]][3])
+                    #print next_cl_id, req_id
+                    if next_cl_id == req_id:
+                        next_state = [self.states[ns_p_a[0]], next_id, ns_p_a[2]]   # state[batter, charging, time, cluster], action to get to this state
+                        return next_state                                  # rewards that could be achieved in this state
+            
+                    next_id = next_id + 1
+
+            else:
+                ns_p_a = possible_states[0]
+                next_state = [self.states[ns_p_a[0]], next_id, ns_p_a[2]]
+                return next_state
+        
+        else:
+            next_state = [self.states[possible_states[-1][0]], len(possible_states)-1,  possible_states[-1][2]]
+            return next_state
