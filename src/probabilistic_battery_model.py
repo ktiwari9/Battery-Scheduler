2aef3223d32e770e5231eaeef05030efce9c5c80
milanmt <milanmariya@gmail.com>
2018-11-04 01:00:36 +0530

    Organising code for modularity.

--- src/probabilistic_battery_model.py
+++ src/probabilistic_battery_model.py
@@ -27,67 +27,100 @@ class BatteryData:
                 self.is_charging = arr[8] == '1'
                 self.date_time = datetime.fromtimestamp(self.epoch)
        
-def get_files(directories):
-    to_process = []
-    for directory in directories:
-        if not os.path.isdir(directory):
-            print("Path {0} is not a directory.".format(directory))
-            return
 
-        print("Processing directory {0}".format(directory))
+class BatteryModel:
+    def __init__(self, directory_paths):
+        self.charge_model = None
+        self.discharge_model = None 
+        self.get_battery_model(directory_paths)
 
-        for root, dirs, files in os.walk(directory):
-            day_files = []
-            if files:
-                # reverse to ensure correct alphanumeric order
-                day_files.extend(reversed([os.path.abspath(os.path.join(root, f)) for f in files if f.endswith('.txt')]))
-                to_process.append(day_files)
-
-    print("Total {0} days".format(len(to_process)))
-    return to_process
+    def get_files(self, directories):
+        to_process = []
+        for directory in directories:
+            if not os.path.isdir(directory):
+                print("Path {0} is not a directory.".format(directory))
+                return
+            print("Processing directory {0}".format(directory))
+            for root, dirs, files in os.walk(directory):
+                day_files = []
+                if files:
+                    # reverse to ensure correct alphanumeric order
+                    day_files.extend(reversed([os.path.abspath(os.path.join(root, f)) for f in files if f.endswith('.txt')]))
+                    to_process.append(day_files)
+        print("Total {0} days".format(len(to_process)))
+        return to_process
     
-def extract_data(files):
-    print ('Extracting data....')
-    for file_set in files:
-        data_battery = []
-        data_timestamp = []
-        data_ischarging = []
-        data_day = []
-        data_time = []
-        for d_file in file_set:
-            print (d_file)
-            if file_set == files[-1]:
-                f = open(d_file, 'r')
-            else:
-                f = open(d_file, mode='r', encoding = "ISO-8859-1") 
-            for line in f.readlines():
-                if '%' not in line and '#' not in line:
-                    bd = BatteryData(line)
-                    if hasattr(bd, 'life'):
-                        data_battery.append(bd.life)
-                        data_timestamp.append(bd.date_time)
-                        data_ischarging.append(bd.is_charging)
-                        data_day.append(bd.date_time.date())
-                        data_time.append(bd.date_time.time())
+    def extract_data(self, files):
+        print ('Extracting data....')
+        for file_set in files:
+            data_battery = []
+            data_timestamp = []
+            data_ischarging = []
+            data_day = []
+            data_time = []
+            for d_file in file_set:
+                if file_set == files[-1]:
+                    f = open(d_file, 'r')
+                else:
+                    f = open(d_file, mode='r', encoding = "ISO-8859-1") 
+                for line in f.readlines():
+                    if '%' not in line and '#' not in line:
+                        bd = BatteryData(line)
+                        if hasattr(bd, 'life'):
+                            data_battery.append(bd.life)
+                            data_timestamp.append(bd.date_time)
+                            data_ischarging.append(bd.is_charging)
 
-        battery_data = pd.DataFrame(data=list(zip(data_battery, data_ischarging, data_time, data_day)), index=data_timestamp, columns= ['life', 'is_charging', 'time', 'day'])
-        battery_data.sort_index(inplace=True)
-        battery_data.drop_duplicates(subset='time', keep='last', inplace=True)
-        battery_data.set_index(pd.DatetimeIndex(battery_data.index), drop=False, inplace=True, verify_integrity=True)
-        battery_data = battery_data.resample('30T').pad()
-        battery_charge = battery_data[battery_data['is_charging'] == True]
-        battery_discharge = battery_data[battery_data['is_charging'] == False]
-        # if not battery_discharge.empty:
-        #     print (battery_discharge)
-        # if not battery_charge.empty:
-        #     print (battery_charge) 
+            battery_data = pd.DataFrame(data=list(zip(data_battery, data_ischarging, data_timestamp)), index=data_timestamp, columns= ['life', 'is_charging', 'time'])
+            battery_data.sort_index(inplace=True)
+            battery_data.drop_duplicates(subset='time', keep='last', inplace=True)
+            battery_data.set_index(pd.DatetimeIndex(battery_data.index), drop=False, inplace=True, verify_integrity=True)
+            battery_data = battery_data.resample('30T').pad()
+            battery_charge = battery_data[battery_data['is_charging'] == True]
+            battery_discharge = battery_data[battery_data['is_charging'] == False]
+            # if not battery_discharge.empty:
+            #     self.__update_discharging_model(battery_discharge)
+            # if not battery_charge.empty:
+            #     self.__update_charging_model(battery_charge)
+        
 
-        ## update the probabilistic model
-    return battery_data
+        ### CHECK CODE TILL HERE 
+        ### UPDATE CODE 
+
+    def __update_charging_model(self,charge_data):
+
+    def __update_discharging_model(self,discharge_data):
+
     
-def get_battery_model
+    def get_battery_model(self, paths):
+        ################ SPECIFY PATHS #######################
+        path = '/home/milan/workspace/strands_ws/src/battery_scheduler'
+        if os.path.isfile(path+'/models/battery_charge_model.yaml') and os.path.isfile(path+'/models/battery_discharge_model.yaml'):
+            with open (path+'/models/battery_charge_model.yaml', 'r') as f_charge:
+                self.charge_model = yaml.load(f_charge)
+            with open (path+'/models/battery_discharge_model.yaml', 'r') as f_discharge:
+                self.discharge_model = yaml.load(f_discharge)
+            print ('Battery Models Found at: ' path+'/models/battery_discharge_model.yaml', path+'/models/battery_charge_model.yaml' )
+
+        else:
+            self.charge_model = dict()
+            self.discharge_model = dict()
+            for model in [charge_model, discharge_model]:
+                for i in range (101):
+                    model.update({ i : dict()})
+
+            files = self.get_files(paths)
+            self.extract_data(files)
+
+            f_discharge = file(path+'/models/battery_discharge_model.yaml', 'w')
+            yaml.dump(self.discharge_model, f_discharge)
+            f_charge =file(path+ '/models/battery_charge_model.yaml', 'w')
+            yaml.dump(self.charge_model, f_charge)
+            print ('Battery Models Created at: ' path+'/models/battery_discharge_model.yaml', path+'/models/battery_charge_model.yaml')
+
     
 if __name__ == '__main__':
+    ################ SPECIFY PATHS #######################
     paths = ['/media/milan/DATA/data_project/battery_data/betty','/media/milan/DATA/data_project/battery_data/bob', '/media/milan/DATA/battery_logs/real_battery']
-    files = get_files(paths)
-    dated_data = extract_data(files)
+    bm = BatteryModel(paths)
+   
