ab62af986669eca8cb4d17842be96c8b8a3c1229
milanmt <milanmariya@gmail.com>
2018-11-04 13:10:00 +0530

    Obtained probabilistic battery models in yaml files as before.

--- src/probabilistic_battery_model.py
+++ src/probabilistic_battery_model.py
@@ -3,15 +3,10 @@
 from datetime import datetime, time, date 
 import pandas as pd
 from io import open
-import sys
-import os
-import time
-import numpy as np
-import math
 import yaml
+import os
 
 class BatteryData:
-    # parsing for old data
     def __init__(self, battery_str):
         arr =  battery_str[:-1].split(',')
         if len(arr) == 6:
@@ -51,7 +46,7 @@ class BatteryModel:
         return to_process
     
     def extract_data(self, files):
-        print ('Extracting data....')
+        print ('Extracting data and forming model....')
         for file_set in files:
             data_battery = []
             data_timestamp = []
@@ -75,52 +70,56 @@ class BatteryModel:
             battery_data.sort_index(inplace=True)
             battery_data.drop_duplicates(subset='time', keep='last', inplace=True)
             battery_data.set_index(pd.DatetimeIndex(battery_data.index), drop=False, inplace=True, verify_integrity=True)
-            battery_data = battery_data.resample('30T').pad()
+            battery_data = battery_data.resample('1T').pad()
             battery_charge = battery_data[battery_data['is_charging'] == True]
             battery_discharge = battery_data[battery_data['is_charging'] == False]
-            # if not battery_discharge.empty:
-            #     self.__update_discharging_model(battery_discharge)
-            # if not battery_charge.empty:
-            #     self.__update_charging_model(battery_charge)
-        
-
-        ### CHECK CODE TILL HERE 
-        ### UPDATE CODE 
-
-    def __update_charging_model(self,charge_data):
-
-    def __update_discharging_model(self,discharge_data):
-
+            if not battery_discharge.empty:
+                self.__update_battery_model(battery_discharge['life'], charging=False)
+            if not battery_charge.empty:
+                self.__update_battery_model(battery_charge['life'],charging=True)
+           
+    def __update_battery_model(self,df,charging=True):
+        if charging:
+            model = self.charge_model
+        else:
+            model = self.discharge_model
+        for i in range(0,df.shape[0]-31):
+            if (df.index[i+30] - df.index[i]) == pd.Timedelta(minutes=30):
+                if df[i+30] not in model[df[i]]:
+                    model[df[i]].update({df[i+30]:1})
+                else:
+                   model[df[i]][df[i+30]] += 1
     
     def get_battery_model(self, paths):
-        ################ SPECIFY PATHS #######################
+        ################ SPECIFY PATHS OF MODELS #######################
         path = '/home/milan/workspace/strands_ws/src/battery_scheduler'
         if os.path.isfile(path+'/models/battery_charge_model.yaml') and os.path.isfile(path+'/models/battery_discharge_model.yaml'):
             with open (path+'/models/battery_charge_model.yaml', 'r') as f_charge:
                 self.charge_model = yaml.load(f_charge)
             with open (path+'/models/battery_discharge_model.yaml', 'r') as f_discharge:
                 self.discharge_model = yaml.load(f_discharge)
-            print ('Battery Models Found at: ' path+'/models/battery_discharge_model.yaml', path+'/models/battery_charge_model.yaml' )
+            print ('Battery Models Found at: ' +path+'/models/battery_discharge_model.yaml'+', '+ path+'/models/battery_charge_model.yaml' )
 
         else:
             self.charge_model = dict()
             self.discharge_model = dict()
-            for model in [charge_model, discharge_model]:
+            for model in [self.charge_model, self.discharge_model]:
                 for i in range (101):
                     model.update({ i : dict()})
 
             files = self.get_files(paths)
             self.extract_data(files)
 
-            f_discharge = file(path+'/models/battery_discharge_model.yaml', 'w')
-            yaml.dump(self.discharge_model, f_discharge)
-            f_charge =file(path+ '/models/battery_charge_model.yaml', 'w')
-            yaml.dump(self.charge_model, f_charge)
-            print ('Battery Models Created at: ' path+'/models/battery_discharge_model.yaml', path+'/models/battery_charge_model.yaml')
+            with open(path+'/models/battery_discharge_model.yaml', 'w') as f_discharge:
+                yaml.dump(self.discharge_model, f_discharge)
+            with open(path+ '/models/battery_charge_model.yaml', 'w') as f_charge:
+                yaml.dump(self.charge_model, f_charge)
+            print ('Battery Models Created at: '+path+'/models/battery_discharge_model.yaml'+ ', '+ path+'/models/battery_charge_model.yaml')
 
-    
+
 if __name__ == '__main__':
-    ################ SPECIFY PATHS #######################
+    ################ SPECIFY PATHS OF DATA #######################
     paths = ['/media/milan/DATA/data_project/battery_data/betty','/media/milan/DATA/data_project/battery_data/bob', '/media/milan/DATA/battery_logs/real_battery']
     bm = BatteryModel(paths)
-   
+    # print (bm.charge_model)
+    # print (bm.discharge_model)
