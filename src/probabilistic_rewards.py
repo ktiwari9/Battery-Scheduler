9dfa76a689e491bbde0822b7363a55999a9afd71
milanmt <milanmariya@gmail.com>
2018-11-11 23:22:59 +0530

    Correcting error in model. Calculating total rewards, day wise.

--- src/probabilistic_rewards.py
+++ src/probabilistic_rewards.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.mixture import BayesianGaussianMixture
 from datetime import time, timedelta, datetime, date
 
+
 def timing_wrapper(func):
     def wrapper(*args,**kwargs):
         t = datetime.now()
@@ -17,6 +18,7 @@ def timing_wrapper(func):
         return result
     return wrapper
 
+
 class uncertain_rewards:
     @timing_wrapper
     def __init__(self):
@@ -24,44 +26,85 @@ class uncertain_rewards:
         # t = read_task_wo_priorities.getTasks()
         self.tasks = tasks_processor.tasks_df
         self.time_int = 30 #minutes
-        self.priorities_as_states, self.state_means = self.__cluster_rewards()
-        self.tasks['reward_states']=pd.Series(self.priorities_as_states)
-        adjusted_start = self.tasks['start_time'].apply(lambda x: datetime.combine(date.today(), x.time()))
-        adjusted_end = self.tasks['end_time_flag'].apply(lambda x: datetime.combine(date.today(), x[0].time()) if x[1] == False else datetime.combine((datetime.today()+timedelta(days=1)).date(), x[0].time()) )
-        self.tasks['adjusted_start'] = adjusted_start
-        self.tasks['adjusted_end'] = adjusted_end
+        self.no_int = 1440/self.time_int
+        self.rewards_day  = dict()
+        self.tasks['start_day'] = self.tasks['start_time'].apply(lambda x: x.date())
+        self.tasks['end_day'] = self.tasks['end_time'].apply(lambda x: x.date())
+        self.test_days = [date(2017, 8, 11), date(2017,8,18)]
+        self.tasks = self.tasks[~self.tasks['start_day'].isin(self.test_days)]
         
     def __cluster_rewards(self):
-        X = np.array(self.tasks['priority']).reshape(-1,1)
+        X = np.array([rew for day,rew in self.rewards_day.items()]).reshape(-1,1)
         dpgmm = BayesianGaussianMixture(n_components=10,max_iter= 500,covariance_type='spherical', random_state=0).fit(X)
-        priorities_as_states = dpgmm.predict(X)
-        reward_states = [m[0] for m in dpgmm.means_]  ## Means indxed by state
+        rews_as_states = dpgmm.predict(X) 
+       
+        for e,el in enumerate(list(X)):
+            if el == [0]:
+                rews_as_states[e] = len(dpgmm.means_)
 
-        # states = np.unique(priorities_as_states)
-        # print ('Number of states: ', len(np.unique(priorities_as_states)), states)
-        # print (dpgmm.means_)
-        # color=['navy', 'c', 'cornflowerblue', 'gold','darkorange', 'r', 'g', 'm', 'y', 'k']
-        # color_labels = []
-        # for label in priorities_as_states:
-        #     color_labels.append(color[int(label)])
-        # print ([ color[s] for s in states])
-        # plt.scatter(range(len(X)), X, color= color_labels)
-        # plt.show()
-        return priorities_as_states, reward_states
+        mean_dict = dict()
+        for e,s in enumerate(rews_as_states):
+            if s not in mean_dict:
+                mean_dict.update({s : [X[e][0]]})
+            else:
+                mean_dict[s].append(X[e][0])
 
+        reward_states = np.zeros((dpgmm.means_.shape[0]+1))
+        for state, vals in mean_dict.items():
+            reward_states[int(state)] = np.mean(vals)
+ 
+        rews_as_states = rews_as_states.reshape(len(self.rewards_day),self.no_int)
+        return rews_as_states, reward_states
+
+    def __update_rewards_day(self, tasks, day): 
+        max_end_day = tasks['end_day'].max()
+        delta_days = (max_end_day - day).days
+        for i in range (delta_days+1):
+            rew_sum_day = np.zeros((self.no_int))
+            start_int = datetime.combine(day,time(0,0))
+            for i in range(self.no_int):
+                end_int = start_int + timedelta(minutes=self.time_int)
+                rew_sum_day[i] = self.tasks[(self.tasks['start_time'] < end_int) & (self.tasks['end_time'] > start_int)]['priority'].sum()
+                start_int = end_int
+            if day not in self.rewards_day:
+                self.rewards_day.update({ day : rew_sum_day})
+            else:
+                self.rewards_day[day] += rew_sum_day
+            day = day + timedelta(days=1)
+
+    @timing_wrapper
     def get_probabilistic_reward_model(self):
-        no_int = 1440/self.time_int   # 1440 is total minutes in a day
-        start_int = datetime.combine(date.today(),time(0,0)) 
-        reward_prob = np.zeros((no_int,len(self.state_means)))   
-        for i in range(no_int):
-            end_int = start_int + timedelta(minutes=self.time_int)
-            df_t = self.tasks[(self.tasks['adjusted_start'] < end_int) & (self.tasks['adjusted_end'] > start_int)]
-            r_unique, r_count = np.unique(df_t['reward_states'], return_counts=True) 
-            r_prob = r_count/float(np.sum(r_count))
-            for r, p in zip(r_unique,r_prob):
-                reward_prob[i,r] = p
-            start_int = end_int
-        return reward_prob, self.state_means
+        days = self.tasks['start_day'].unique().tolist()
+        for day in days:
+            current_tasks = self.tasks[self.tasks['start_day']==day]
+            self.__update_rewards_day(current_tasks, day)
+
+        rewards, reward_states = self.__cluster_rewards()
+        states = list(np.unique(rewards))
+        z_l = np.max(states)
+
+        prob_m = np.zeros((self.no_int, len(states)-1))
+        rewardst = rewards.T
+        for i in range(rewardst.shape[0]):
+            st, st_c = np.unique(rewardst[i], return_counts=True)
+            st = list(st)
+            if z_l in st:
+                z_ind = st.index(z_l)
+                total_c = np.sum(st_c) - st_c[z_ind]
+            else:
+                total_c = np.sum(st_c)
+            
+            st_c = st_c/float(total_c)
+            for j, s in enumerate(st):
+                if s != z_l:
+                    prob_m[i][states.index(s)] = st_c[j]
+
+        state_means = [round(reward_states[int(s)]) for s in states if s != z_l]
+        return prob_m, state_means
+
 
 if __name__ == '__main__':
     ur = uncertain_rewards()
+    prob_m, state_means = ur.get_probabilistic_reward_model()
+    print prob_m
+    print state_means
