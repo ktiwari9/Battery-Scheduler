7304ddcb4b3b93267eeddf6be058d0a42bbaf215
Milan Mariya Tomy <milanmariya@gmail.com>
2019-04-08 22:06:44 +0530

    Updating with code to calculate expected cost daily and to calculate average time spent working and charging continuously.

--- src/plan_statistics.py
+++ src/plan_statistics.py
@@ -10,8 +10,8 @@ THRESHOLD = 40
 
 
 def get_battery_model():
-    # path = '/home/milan/workspace/strands_ws/src/battery_scheduler'
-    path = roslib.packages.get_pkg_dir('battery_scheduler')
+    path = '/home/milan/workspace/strands_ws/src/battery_scheduler'
+    # path = roslib.packages.get_pkg_dir('battery_scheduler')
     if os.path.isfile(path+'/models/battery_charge_model.yaml') and os.path.isfile(path+'/models/battery_discharge_model.yaml'):
         with open (path+'/models/battery_charge_model.yaml', 'r') as f_charge:
             charge_model = yaml.load(f_charge)
@@ -38,6 +38,7 @@ def read_file(fname):
 	actions = []
 	paretos  = []  ## for pareto points
 	actual_reward_present = False
+	print filename
 	with open (filename, 'r') as f:
 		for line in f:
 			x = line[:-1].split(' ')
@@ -48,6 +49,7 @@ def read_file(fname):
 		for line in f:
 			if 'battery' not in line:
 				el = line[:-1].split(' ')
+				print el
 				battery.append(float(el[1].strip()))
 				obtained_rewards.append(float(el[4].strip()))
 				actions.append(el[3].strip())
@@ -82,7 +84,6 @@ def get_battery_statistics(battery, actions):
 	return battery_mean, battery_std, battery_max, battery_min, b_end
 
 def get_reward_statistics(actual_rewards, obtained_rewards):
-	total_rew = None
 	if actual_rewards:
 		total_rew = np.array([sum(actual_rewards[i*TOTAL_INT:(i+1)*TOTAL_INT]) for i in range(len(actual_rewards)/TOTAL_INT)]
 		)
@@ -92,39 +93,47 @@ def get_reward_statistics(actual_rewards, obtained_rewards):
 	hours_worked = np.array([sum(hours_worked_array[i*TOTAL_INT:(i+1)*TOTAL_INT]) for i in range(len(hours_worked_array)/TOTAL_INT)])
 	hours_work_available = np.array([sum(hours_work_available_array[i*TOTAL_INT:(i+1)*TOTAL_INT]) for i in range(len(hours_work_available_array)/TOTAL_INT)])
 
-	if total_rew != None:
+	if actual_rewards:
 		return 100*(total_obtained/total_rew), total_rew, 100*(hours_worked/hours_work_available), hours_work_available
 	else:
 		return total_obtained, None, hours_worked, hours_work_available
 
-def get_continuous_action_time(actions):
-	working_count = []
-	charging_count = []
-	count = 0
-	for e,a in enumerate(actions):
-		if a == 'go_charge' or a == 'stay_charging':
-			count +=1 
-		else:
-			if count != 0:
-				charging_count.append(count)
-			count = 0
+def get_continuous_action_time(all_actions):
+	avg_cont_work_int = []
+	avg_cont_charge_int = []
+	for i in range(len(all_actions)/TOTAL_INT):
+		actions = all_actions[i*TOTAL_INT:(i+1)*TOTAL_INT]
+		working_count = []
+		charging_count = []
+		count = 0
+		for e,a in enumerate(actions):
+			if a == 'go_charge' or a == 'stay_charging':
+				count +=1 
+			else:
+				if count != 0:
+					charging_count.append(count)
+				count = 0
 
-	count = 0
-	for e,a in enumerate(actions):
-		if a == 'gather_reward':
-			count +=1 
-		else:
-			if count != 0:
-				working_count.append(count)
-			count = 0
-	
-	return np.mean(working_count), np.mean(charging_count)
+		count = 0
+		for e,a in enumerate(actions):
+			if a == 'gather_reward':
+				count +=1 
+			else:
+				if count != 0:
+					working_count.append(count)
+				count = 0
+
+		avg_cont_charge_int.append(np.mean(charging_count))
+		avg_cont_work_int.append(np.mean(working_count))
+		
+	return np.array(avg_cont_work_int), np.array(avg_cont_charge_int)
 
 if __name__ == '__main__':
 	# This will give csv file with all necessary values for plotting different kinds of stats for one particular form of control.
 
-	# Set TOTAL_INT at the beginning of file
-	fsets = [['p-1fhc_bcth_sep242526_70b_1', 'p-1fhc_bcth_sep242526_70b_2', 'p-1fhc_bcth_sep242526_70b_3'], ['p-1fhc_bcth_oct123_70b_1', 'p-1fhc_bcth_oct123_70b_2', 'p-1fhc_bcth_oct123_70b_3']]## Can include sets of different days, different initial states.
+	# Set TOTAL_INT at the beginning of file, change filename at end according to model
+	fsets = [['p0rhct_bcth_318110_70b_1', 'p0rhct_bcth_318110_70b_2', 'p0rhct_bcth_318110_70b_3']]
+	## Can include sets of different days, different initial states.
 
 	sets_percent_rew = []                   ## for plotting values accross varied data
 	sets_percent_hours = []
@@ -133,6 +142,7 @@ if __name__ == '__main__':
 	sets_end_battery = []
 	sets_cont_charge = []
 	sets_cont_work = []
+	sets_exp_cost = []
 
 	for fnames in fsets:
 		b_total = []                          ## give array of expected daily values 
@@ -144,6 +154,7 @@ if __name__ == '__main__':
 		overall_cont_charge = []
 		overall_percent_under_th = []
 		overall_end_battery = []
+		overall_expected_cost = []
 
 		for fname in fnames:		
 			battery, actions, obtained_rewards, actual_rewards, paretos = read_file(fname)
@@ -154,7 +165,7 @@ if __name__ == '__main__':
 			total_obtained, total_rew, hours_worked, hours_work_available = get_reward_statistics(actual_rewards, obtained_rewards)
 			overall_hours_available.append(hours_work_available)
 			overall_hours_worked.append(hours_worked)
-			if total_rew != None:
+			if actual_rewards:
 				overall_total_rews.append(total_rew)
 			overall_total_obt.append(total_obtained)
 			
@@ -168,20 +179,27 @@ if __name__ == '__main__':
 			overall_cont_charge.append(avg_cont_charge/2)  ## cont charging in hours
 			overall_cont_work.append(avg_cont_work/2)
 
+			exp_cost = np.array([np.mean(paretos[i*TOTAL_INT:(i+1)*TOTAL_INT]) for i in range(len(battery)/TOTAL_INT)])
+			overall_expected_cost.append(exp_cost)
+
 		overall_end_battery = np.mean(np.array(overall_end_battery), axis=0)
 		overall_total_obt = np.mean(np.array(overall_total_obt), axis=0)
 		overall_hours_worked = np.mean(np.array(overall_hours_worked), axis=0)
 		overall_percent_under_th = np.mean(np.array(overall_percent_under_th), axis=0)
+		overall_cont_work = np.mean(np.array(overall_cont_work), axis=0)
+		overall_cont_charge = np.mean(np.array(overall_cont_charge), axis=0)
+		overall_expected_cost = np.mean(np.array(overall_expected_cost), axis=0)
 	
 		sets_end_battery.extend(list(overall_end_battery))
 		sets_battery.extend(b_total)
 		sets_percent_rew.extend(list(overall_total_obt))
 		sets_percent_hours.extend(list(overall_hours_worked))
 		sets_percent_uth.extend(list(overall_percent_under_th))
-		sets_cont_work.extend(overall_cont_work)
-		sets_cont_charge.extend(overall_cont_charge)
+		sets_cont_work.extend(list(overall_cont_work))
+		sets_cont_charge.extend(list(overall_cont_charge))
+		sets_exp_cost.extend(list(overall_expected_cost))
 
 	df_b = pd.DataFrame(data = list(zip(np.arange(len(sets_battery)), sets_battery)), columns=['nums', 'battery_life'])
 	df_b.to_csv('/home/milan/workspace/strands_ws/src/battery_scheduler/data/csv_files/busy_p-1fhc_bcth_battery_csv.csv', header=False, index=False)
-	df = pd.DataFrame(data = list(zip(sets_end_battery, sets_percent_rew, sets_percent_hours, sets_percent_uth, sets_cont_charge, sets_cont_work)), columns=['end_battery', 'percent_rew', 'percent_hours', 'percent_under_th', 'cont_charge_hrs', 'cont_work_hrs'])
-	df.to_csv('/home/milan/workspace/strands_ws/src/battery_scheduler/data/csv_files/busy_p-1fhc_bcth_csv.csv', header=True, index=True)  ## - change file name according to control, one file name for one control
+	df = pd.DataFrame(data = list(zip(sets_end_battery, sets_percent_rew, sets_percent_hours, sets_percent_uth, sets_cont_charge, sets_cont_work, sets_exp_cost)), columns=['end_battery', 'percent_rew', 'percent_hours', 'percent_under_th', 'cont_charge_hrs', 'cont_work_hrs', 'pareto_cost'])
+	df.to_csv('/home/milan/workspace/strands_ws/src/battery_scheduler/data/csv_files/busy_p-1fhc_bcth_csv.csv', header=True, index=True) ## - change file name according to control, one file name for one control
