c7dc6e81d36be2964408586f71188e906b063159
milanmt <milanmariya@gmail.com>
2019-12-06 02:03:09 +0530

    corresponding to read_tasks.py

--- src/taskbased_sample_generator.py
+++ src/taskbased_sample_generator.py
@@ -14,38 +14,34 @@ class SampleGenerator:
 
         client = pymongo.MongoClient(rospy.get_param("mongodb_host", "localhost"),rospy.get_param("mongodb_port", 62345))
         
-        sample_array = []
-        unique_tasks = []
+        unique_tasks = dict()
         
         for start_t, end_t in self.test_ts:
             tasks = client.message_store.task_events.find({"task.start_after.secs": {'$gte':start_t}, "task.end_before.secs":{'$lt':end_t}})
 
-            prev_start = datetime(1970,1,1)  
             for task in tasks:
                 event = task['task'] 
                 task_id = task['task']['task_id']
-                if task_id not in unique_tasks: 
-                    start = datetime.fromtimestamp(event['start_after']['secs'])
-                    end = datetime.fromtimestamp(event['end_before']['secs'])
-                    priority = event['priority']
-                    # priority = 1
+                start = datetime.fromtimestamp(event['start_after']['secs'])
+                end = datetime.fromtimestamp(event['end_before']['secs'])
+                priority = event['priority']
+                # priority = 1
+            
+                action = str(task['task']['action'])
+                if ('(F' in action) and ('&' in action) and ('|' not in action):
+                    action = 'edge_exploration'
+                elif ('(F' in action) and ('&' in action) and ('|' in action):
+                    action = 'delivery_action'
+                elif action == '':
+                    action = 'un-named_action'
+                elif action[0] == '/':
+                    action = action[1:]
+
+                node = task['task']['start_node_id']
                 
-                    action = str(task['task']['action'])
-                    if ('(F' in action) and ('&' in action) and ('|' not in action):
-                        action = 'edge_exploration'
-                    elif ('(F' in action) and ('&' in action) and ('|' in action):
-                        action = 'delivery_action'
-                    elif action == '':
-                        action = 'un-named_action'
-                    elif action[0] == '/':
-                        action = action[1:]
-
-                    node = task['task']['start_node_id']
-                    
-                    if node != 'ChargingPoint1' and action != 'cpm_action'and action != 'un-named_action' and priority != 0:
-                        sample_array.append((start, end, priority))
-                        unique_tasks.append(task_id)              
-
+                if node != 'ChargingPoint1' and action != 'cpm_action'and action != 'un-named_action' and priority != 0:
+                    unique_tasks.update({task_id:(start, end, priority)})              
+        sample_array = [val for val in unique_tasks.values()]
         sample_array.sort(key=lambda x: x[0])    
         self.samples = pd.DataFrame(data=sample_array, columns=['start', 'end', 'priority'])
         
@@ -76,12 +72,12 @@ class SampleGenerator:
         #         sample_start.append(start)
         #         sample_end.append(end)
         #         sample_priority.append(priority)
-        #         unique_tasks.append(task_id)          
             
         # self.samples = pd.DataFrame(data=zip(sample_start,sample_end,sample_priority), columns=['start', 'end', 'priority'])
         
 
 if __name__ == "__main__":
-    test_days = [datetime(2017, 9, 24), datetime(2017,9,25), datetime(2017, 9, 26)]
-    SampleGenerator (test_days)
+    test_days = [datetime(2017, 10, 1), datetime(2017,10,2), datetime(2017, 10,3)]
+    sg = SampleGenerator (test_days)
+    print sg.samples
     
