4bc7f4e2cf1a9aaef778e1fcb9db5b8399f6d811
milanmt <milanmariya@gmail.com>
2020-04-24 22:34:11 +0300

    Weighted go charge model with weight = 0.99

--- src/bcth_prism_model.py
+++ src/bcth_prism_model.py
@@ -8,6 +8,29 @@ import copy
 import yaml
 import os
 
+def get_gocharge_model(charge_model):
+    gocharge_model = dict ()
+    for b, bdict in charge_model.items():
+        g_bdict = dict()
+        if b == 99 or b == 100:
+            g_bdict = copy.deepcopy(bdict)
+        else:
+            for bn, count in bdict.items():
+                gbn = round(0.99*bn)
+                if gbn > b:
+                    if gbn in g_bdict:
+                        g_bdict[gbn] += count
+                    else:
+                        g_bdict.update({gbn : count})
+                else:
+                    if bn in g_bdict:
+                        g_bdict[bn] += count
+                    else:
+                        g_bdict.update({bn : count})
+        gocharge_model.update({b:g_bdict})
+    return gocharge_model
+
+
 class PrismModel:
     
     def __init__(self,filename, horizon, init_b, init_ch, init_clid, task_prob, clusters, prob, charge_model, discharge_model):
@@ -18,7 +41,9 @@ class PrismModel:
         self.actions = ['gather_reward', 'go_charge', 'stay_charging']
         self.charge_model = charge_model
         self.discharge_model = discharge_model
-        self.time_int = horizon  ## horizon in number of intervals 
+        self.gocharge_model = get_gocharge_model(charge_model)
+        self.time_int = horizon  ## horizon in number of intervals
+
         self.write_prism_file(filename, init_b, init_ch, init_clid) 
         
     def write_prism_file(self, filename, init_b, init_ch, init_clid):
@@ -56,37 +81,17 @@ class PrismModel:
                         plus = plus +1
                     f.write(';\n')
 
-            for b in self.charge_model:
-                if b != 0:
-                    if b == 100 or b == 99:
-                        bnext_dict = self.charge_model[b]
-                        total = np.sum(np.array(bnext_dict.values()))
-                        f.write("[go_charge] (battery={0}) & (t>-1) -> ".format(b))
-                        plus = 0
-                        for bnext, val in bnext_dict.items():
-                            if plus != 0:
-                                f.write(' + ')
-                            f.write("{0}:(battery'={1})".format(float(val)/float(total), int(bnext)))
-                            plus = plus +1
-                        f.write(';\n')
-                    else:
-                        bnext_dict = copy.deepcopy(self.charge_model[b])
-                        if b in bnext_dict:
-                            if b+1 in bnext_dict:
-                                bnext_dict[b+1] += bnext_dict[b]
-                            else:
-                                bnext_dict[b+1] = bnext[b]
-                            del bnext_dict[b]
-
-                        total = np.sum(np.array(bnext_dict.values()))
-                        f.write("[go_charge] (battery={0}) & (t>-1) -> ".format(b))
-                        plus = 0
-                        for bnext, val in bnext_dict.items():
-                            if plus != 0:
-                                f.write(' + ')
-                            f.write("{0}:(battery'={1})".format(float(val)/float(total), int(bnext-1)))
-                            plus = plus +1
-                        f.write(';\n')
+            for b in self.gocharge_model:
+                bnext_dict = self.gocharge_model[b]
+                total = np.sum(np.array(bnext_dict.values()))
+                f.write("[go_charge] (battery={0}) & (t>-1) -> ".format(b))
+                plus = 0
+                for bnext, val in bnext_dict.items():
+                    if plus != 0:
+                        f.write(' + ')
+                    f.write("{0}:(battery'={1})".format(float(val)/float(total), int(bnext)))
+                    plus = plus +1
+                f.write(';\n')
 
             for b in self.charge_model:
                 bnext_dict = self.charge_model[b]
@@ -143,12 +148,12 @@ class PrismModel:
 
 def get_battery_model():
     path = roslib.packages.get_pkg_dir('battery_scheduler')
-    if os.path.isfile(path+'/models/battery_charge_modelm.yaml') and os.path.isfile(path+'/models/battery_discharge_modelm.yaml'):
-        with open (path+'/models/battery_charge_modelm.yaml', 'r') as f_charge:
+    if os.path.isfile(path+'/models/battery_charge_model.yaml') and os.path.isfile(path+'/models/battery_discharge_model.yaml'):
+        with open (path+'/models/battery_charge_model.yaml', 'r') as f_charge:
             charge_model = yaml.load(f_charge)
-        with open (path+'/models/battery_discharge_modelm.yaml', 'r') as f_discharge:
+        with open (path+'/models/battery_discharge_model.yaml', 'r') as f_discharge:
             discharge_model = yaml.load(f_discharge)
-        print ('Battery Models Found at: ' +path+'/models/battery_discharge_modelm.yaml'+', '+ path+'/models/battery_charge_modelm.yaml' )
+        print ('Battery Models Found at: ' +path+'/models/battery_discharge_model.yaml'+', '+ path+'/models/battery_charge_model.yaml' )
         return charge_model, discharge_model
     else:
         raise ValueError('No models found. First create battery model with probabilistic_battery_modelm.py')
@@ -158,3 +163,9 @@ if __name__ == '__main__':
     task_prob, prob, clusters = ur.get_probabilistic_reward_model()
     charge_model, discharge_model = get_battery_model()
     mm = PrismModel('bcth_model.prism', 48, 70, 1, 1, task_prob, clusters, prob, charge_model, discharge_model)
+
+    # mm = PrismModel('bcth12_model.prism', 12, 70, 1, 1, task_prob, clusters, prob, charge_model, discharge_model)
+
+    # mm = PrismModel('bcth24_model.prism', 24, 70, 1, 1, task_prob, clusters, prob, charge_model, discharge_model)
+
+    # mm = PrismModel('bcth36_model.prism', 36, 70, 1, 1, task_prob, clusters, prob, charge_model, discharge_model)
