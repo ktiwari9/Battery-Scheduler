d683fb666a75b99cbe94b34cefc6c9f3fd97e567
Milan Mariya Tomy <milanmariya@gmail.com>
2020-06-06 22:26:39 +0300

    correcting errors when no data to cluster

--- src/sw_probabilistic_rewards_t.py
+++ src/sw_probabilistic_rewards_t.py
@@ -67,39 +67,59 @@ class ProbabilisticRewards:
     def __cluster_rewards(self):
         X = np.array([rew for day,rew in self.rewards_day.items()]).reshape(-1,1)
         X_wz = np.array([x for x in X if x[0] != 0])
-        if self.no_clusters == 'auto':
-            self.dpgmm = BayesianGaussianMixture(n_components=10,max_iter= 700,covariance_type='spherical', random_state=0).fit(X_wz)
-        else:
-            self.dpgmm = KMeans(n_clusters=self.no_clusters).fit(X)
         
-        rews_as_states = self.dpgmm.predict(X) 
-        for e,el in enumerate(list(X)):
-            if el == [0]:
-                if self.no_clusters == 'auto':
-                    rews_as_states[e] = len(self.dpgmm.means_)
-                else:
-                    rews_as_states[e] = len(self.dpgmm.cluster_centers_)
+        if len(X_wz) > 1:
+            if len(X_wz) >=10:
+                n_comps = 10
+            elif len(X_wz) >= 2 and len(X_wz) < 10:  ## value of n_components
+                n_comps = len(X_wz)
 
-        mean_dict = dict()
-        for e,s in enumerate(rews_as_states):
-            if s not in mean_dict:
-                mean_dict.update({s : [X[e][0]]})
+            if self.no_clusters == 'auto':
+                self.dpgmm = BayesianGaussianMixture(n_components=n_comps,max_iter= 700,covariance_type='spherical', random_state=0).fit(X_wz)
             else:
-                mean_dict[s].append(X[e][0])
+                self.dpgmm = KMeans(n_clusters=self.no_clusters).fit(X)
 
-        if self.no_clusters == 'auto':
-            reward_states = np.zeros((self.dpgmm.means_.shape[0]+1))
+            rews_as_states = self.dpgmm.predict(X) 
+            for e,el in enumerate(list(X)):
+                if el == [0]:
+                    if self.no_clusters == 'auto':
+                        rews_as_states[e] = len(self.dpgmm.means_)
+                    else:
+                        rews_as_states[e] = len(self.dpgmm.cluster_centers_)
+
+            mean_dict = dict()
+            for e,s in enumerate(rews_as_states):
+                if s not in mean_dict:
+                    mean_dict.update({s : [X[e][0]]})
+                else:
+                    mean_dict[s].append(X[e][0])
+
+            if self.no_clusters == 'auto':
+                reward_states = np.zeros((self.dpgmm.means_.shape[0]+1))
+            else:
+                reward_states = np.zeros((self.dpgmm.cluster_centers_.shape[0]+1))
+
+            for state, vals in mean_dict.items():
+                reward_states[int(state)] = np.mean(vals)
+
+            # print reward_states, 'manual mean'
+            # print self.dpgmm.means_, 'gmm means'
+
+            rews_as_states = rews_as_states.reshape(len(self.rewards_day),self.no_int)
+            return rews_as_states, reward_states
+
         else:
-            reward_states = np.zeros((self.dpgmm.cluster_centers_.shape[0]+1))
-
-        for state, vals in mean_dict.items():
-            reward_states[int(state)] = np.mean(vals)
-
-        # print reward_states, 'manual mean'
-        # print self.dpgmm.means_, 'gmm means'
- 
-        rews_as_states = rews_as_states.reshape(len(self.rewards_day),self.no_int)
-        return rews_as_states, reward_states
+            if len(X_wz) == 1:
+                rews_as_states = []
+                for el in list(X):
+                    if el != 0:
+                        rews_as_states.append(0)
+                    else:
+                        rews_as_states.append(1)
+                return rews_as_states, X_wz
+            else:
+                rews_as_states = np.zeros((len(X)))
+                return rews_as_states, [0]
 
     def __update_rewards_day(self, tasks, model_start): 
         max_end_day = tasks['end_day'].max()
@@ -134,32 +154,38 @@ class ProbabilisticRewards:
 
             rewards, reward_states = self.__cluster_rewards()
             states = list(np.unique(rewards))
-            z_l = np.max(states)
+            
+            if len(states) !=0:
+                z_l = np.max(states)
 
-            prob_m = np.zeros((self.no_int, len(states)-1))
-            task_prob = np.zeros((self.no_int, 2))
-            rewardst = rewards.T
-            for i in range(rewardst.shape[0]):
-                st, st_c = np.unique(rewardst[i], return_counts=True)
-                st = list(st)
-                if z_l in st:
-                    z_ind = st.index(z_l)
-                    total_c = np.sum(st_c) - st_c[z_ind]
-                    
-                    task_prob[i][0] = float(st_c[z_ind])/np.sum(st_c)
-                    task_prob[i][1] = float(total_c)/np.sum(st_c)
-                else:
-                    total_c = np.sum(st_c)
-                    task_prob[i][1] = 1
+                prob_m = np.zeros((self.no_int, len(states)-1))
+                task_prob = np.zeros((self.no_int, 2))
+                rewardst = rewards.T
+                for i in range(rewardst.shape[0]):
+                    st, st_c = np.unique(rewardst[i], return_counts=True)
+                    st = list(st)
+                    if z_l in st:
+                        z_ind = st.index(z_l)
+                        total_c = np.sum(st_c) - st_c[z_ind]
+
+                        task_prob[i][0] = float(st_c[z_ind])/np.sum(st_c)
+                        task_prob[i][1] = float(total_c)/np.sum(st_c)
+                    else:
+                        total_c = np.sum(st_c)
+                        task_prob[i][1] = 1
+
+                    st_c = st_c/float(total_c)
+                    for j, s in enumerate(st):
+                        if s != z_l:
+                            prob_m[i][states.index(s)] = st_c[j]
+
+                state_means = [round(reward_states[int(s)]) for s in states if s != z_l]
                 
-                st_c = st_c/float(total_c)
-                for j, s in enumerate(st):
-                    if s != z_l:
-                        prob_m[i][states.index(s)] = st_c[j]
+            else:
+                prob_m = np.ones((self.no_int,1))
+                state_means = [0]
+                task_prob = np.array(self.no_int*[[1,0]])
 
-            state_means = [round(reward_states[int(s)]) for s in states if s != z_l]
-            return task_prob, prob_m, state_means
-    
         else:
             ## expect tasks of uniform rewards (500) from 8.00 am to 10.00pm
             prob_m = np.ones((self.no_int,1))
@@ -171,7 +197,9 @@ class ProbabilisticRewards:
                 else:
                     task_prob[i][0] = 1
                 timestamp = timestamp + timedelta(minutes=self.int_duration)
-            return task_prob, prob_m, state_means
+         
+        print state_means   
+        return task_prob, prob_m, state_means
         
         
 if __name__ == "__main__":
